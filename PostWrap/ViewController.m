6eed8aa22bec6a714da595c004ff266fcd6a2344
shalini90 <pshalini@shalini-macp.local>
2016-02-16 18:15:18 +0530

    1.Image preview button - done
    2.Plus button - done
    
    3.Swipe to delete - done

--- PostWrap/ViewController.m
+++ PostWrap/ViewController.m
@@ -8,7 +8,6 @@
 
 #import "ViewController.h"
 #import <QuartzCore/QuartzCore.h>
-#import "PWTextInputTableViewCell.h"
 #import "PWEditViewController.h"
 
 @interface ViewController ()
@@ -17,29 +16,128 @@
 @property(nonatomic,strong) UITapGestureRecognizer *tapGestureRecognizer;
 @property (strong, nonatomic) IBOutlet UITextView *contentTextView;
 @property (strong, nonatomic) IBOutlet UIImageView *contentImageView;
-@property (nonatomic,strong) PWTextInputTableViewCell *textInputTableViewCell;
 @property (strong, nonatomic) IBOutlet UIButton *createPosterbtn;
 @property (nonatomic,strong) UIImage *cellImage;
+@property (nonatomic,strong) IBOutlet UITableView *tableView;
+@property (nonatomic,strong) NSMutableArray *inputArray;
+
+@property (nonatomic,strong) PWTextInputTableViewCell *textInputTableViewCell;
+
 @end
 
-static NSString *cellID = @"PWTextInputTableViewCell";
 
 
 @implementation ViewController
 
+int rowCount = 1;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(imageViewtapped:)];
     self.tapGestureRecognizer.delegate = self;
-    self.cellImage = [UIImage imageNamed:@"Camera_icon.png"];
-    self.contentImageView.image=self.cellImage;
+    // self.cellImage = [UIImage imageNamed:@"Camera_icon.png"];
+    //  self.contentImageView.image=self.cellImage;
     [self.contentImageView addGestureRecognizer:self.tapGestureRecognizer];
-    self.automaticallyAdjustsScrollViewInsets=NO;
+    //  self.automaticallyAdjustsScrollViewInsets=NO;
     self.contentTextView.layer.cornerRadius = 5.0;
     self.contentImageView.layer.cornerRadius = 10.0;
     self.createPosterbtn.layer.cornerRadius = 5.0;
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"PWTextInputTableViewCell" bundle:nil] forCellReuseIdentifier:@"PWTextInputTableViewCell"];
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    self.inputArray = [[NSMutableArray alloc]init];
+    self.tableView.allowsSelection = NO;
+    self.tableView.backgroundColor = [UIColor clearColor];
+    
+}
+
+// UITableView Delegates
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    PWTextInputTableViewCell *textInputTableViewCell = nil;
+    textInputTableViewCell.selectionStyle = UITableViewCellSelectionStyleNone;
+    textInputTableViewCell = [tableView dequeueReusableCellWithIdentifier:@"PWTextInputTableViewCell"];
+    textInputTableViewCell.delegate = self;
+    textInputTableViewCell.inputTextView.delegate = self;
+    textInputTableViewCell.backgroundColor = [UIColor clearColor];
+    textInputTableViewCell.inputTextView.tag = indexPath.row;
+    // textInputTableViewCell.inputTextView.text = @"Enter your text here";
+    return textInputTableViewCell;
+    
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return rowCount;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
+    
+    return 50.0;
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return YES if you want the specified item to be editable.
+    return YES;
+}
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        rowCount --;
+        //    [self.inputArray removeObjectAtIndex:indexPath.row];
+        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+    [self.tableView reloadData];
+}
+
+
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+    
+    
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView{
+    
+    //   NSString * textToAdd =
+    
+    [self.inputArray removeObject:textView.text];
+    
+}
+
+
+-(IBAction)addButtonPressed:(id)sender{
+    
+    NSLog(@"%@ text",self.textInputTableViewCell.inputTextView.text);
+    
+    
+    if (rowCount < 3) {
+        rowCount++;
+        [self.tableView beginUpdates];
+        
+        NSArray *paths = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:rowCount-1 inSection:0]];
+        
+        //    [self.tableView insertRowsAtIndexPaths:paths withRowAnimation:UITableViewRowAnimationFade];
+        [self.tableView insertRowsAtIndexPaths:paths withRowAnimation:UITableViewRowAnimationFade];
+        //
+        [self.tableView endUpdates];
+        //  self.textInputTableViewCell.inputTextView.text = nil;
+        [self.tableView reloadData];
+        [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:rowCount -1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    }
+    
+    //    for (int i=0 ; i<rowCount; i++){
+    //        [self.inputArray addObject:self.textInputTableViewCell.inputTextView.text];
+    //    }
 }
 
 
@@ -66,11 +164,11 @@ static NSString *cellID = @"PWTextInputTableViewCell";
     
     // Gallery Button
     actionGallery = [UIAlertAction
-                    actionWithTitle:NSLocalizedString(@"Choose from Gallery", nil)
-                    style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                        [self openGallery];
-                        
-                    }];
+                     actionWithTitle:NSLocalizedString(@"Choose from Gallery", nil)
+                     style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                         [self openGallery];
+                         
+                     }];
     
     // Cancel Button
     
@@ -104,7 +202,7 @@ static NSString *cellID = @"PWTextInputTableViewCell";
     picker.delegate = self;
     [picker setSourceType:(UIImagePickerControllerSourceTypePhotoLibrary)];
     [self presentViewController:picker animated:YES completion:Nil];
-
+    
 }
 
 
@@ -114,7 +212,7 @@ static NSString *cellID = @"PWTextInputTableViewCell";
     
     UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
     [picker dismissViewControllerAnimated:YES completion:nil];
-    self.cellImage = image;
+    self.contentImageView.image = image;
     
 }
 
@@ -124,7 +222,7 @@ static NSString *cellID = @"PWTextInputTableViewCell";
     if ([segue.identifier isEqualToString:@"showEditScreen"])
     {
         PWEditViewController *editViewController = segue.destinationViewController;
-        editViewController.bgImage = self.cellImage;
+        editViewController.bgImage = self.contentImageView.image;
         editViewController.textArray = [[NSMutableArray alloc]initWithObjects:self.contentTextView.text, nil];
     } }
 
