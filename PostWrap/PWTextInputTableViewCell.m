c359e6947ff5111bf835884279d975b4d714a631
shalini90 <pshalini@shalini-macp.local>
2016-02-16 18:35:15 +0530

    HomeScreen

--- PostWrap/PWTextInputTableViewCell.m
+++ PostWrap/PWTextInputTableViewCell.m
@@ -13,19 +13,31 @@
 
 - (void)awakeFromNib {
     // Initialization code
+    self.inputTextView.layer.cornerRadius = 2.0;
+    self.inputTextView.text = @"Enter your text here";
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
 
+//-(void)prepareForReuse{
+//
+//    [super prepareForReuse];
+//    self.inputTextView.text = @"Enter your text here";
+//}
 
 -(void)textViewDidEndEditing:(UITextView *)textView{
     
     self.textInputArray = [[NSMutableArray alloc]initWithObjects:textView.text, nil];
     [textView resignFirstResponder];
 };
+
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+    
+    
+}
 
 @end
